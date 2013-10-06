a621ba2fbf1bfc533b4a6983c37a7991244d681a
Colm Hally <hallyuniversal@gmail.com>
2013-10-06 23:33:02 +0100

    Initial Commit

---
+++ CJHCircularProgressView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CJHCircularProgressView
+//
+//  Created by Colm Hally on 06/10/2013.
+//  Copyright (c) 2013 Colm Hally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CJHCircularProgressViewTests/CJHCircularProgressViewTests.m
@@ -0,0 +1,34 @@
+//
+//  CJHCircularProgressViewTests.m
+//  CJHCircularProgressViewTests
+//
+//  Created by Colm Hally on 06/10/2013.
+//  Copyright (c) 2013 Colm Hally. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CJHCircularProgressViewTests : XCTestCase
+
+@end
+
+@implementation CJHCircularProgressViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
